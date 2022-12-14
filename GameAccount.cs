3e0c8b2df546136171c4c295cf8977c72d2ccf27
nata-ko <99805828+nata-ko@users.noreply.github.com>
2022-12-14 11:56:16 +0200

    added private field and properties for them

--- GameAccount.cs
+++ GameAccount.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,40 +8,80 @@ namespace Lab1
 {
     internal class GameAccount
     {
-        public string UserName { get; set; }
-        public bool isVip = false;
-        public bool isCheater = false;
-        public int CurrentRating { get; set; }
-        private List<History> gameAccountStatus = new List<History>();
+        private bool _isVip = false;
+        public bool IsVip { get => _isVip ;
+            set
+            {
+                _isVip = value;
+            } 
+        }
+
+        private bool _isCheater = false;
+        public bool IsCheater
+        {
+            get => _isCheater;
+            set
+            {
+                _isCheater = value;
+            }
+        }
+        private int _currentRating;
+        private String _userName;
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                _userName = value;
+            }
+        }
+        public int CurrentRating
+        {
+            set
+            {
+                if(_currentRating <= 0)
+                {
+                    _currentRating = 1;
+                }
+                else
+                {
+                    _currentRating = value;
+                }
+                
+            }
+
+            get => _currentRating;
+        }
+        private readonly List<History> _gameAccountStatus = new List<History>();
 
 
 
         public GameAccount(string name)
         {
-            UserName = name;
-            CurrentRating = new Random().Next(1, 20);
+            _userName = name;
+            _currentRating = new Random().Next(1, 20);
         }
 
 
-        public static bool CheckRating(GameAccount user, int gameRating)
+        public  bool CheckRating( int gameRating)
         {
-            return user.CurrentRating - gameRating > 0;
+            return _currentRating - gameRating > 0;
         }
 
 
-        public static void  GetExeption(Boolean checkRating, GameAccount player)
+        public  void  GetExeption(Boolean checkRating)
         {
             if (!checkRating)
             {
-                Console.WriteLine($"\t\t\tERROR! {player.UserName}'s rating is less than 0! UPDATED {player.UserName}'S RATING = 1");
+                Console.WriteLine($"\t\t\tERROR! {_userName}'s rating is less than 0! UPDATED {_userName}'S RATING = 1");
             }
         }
         public string GetStatus()
             {
                 var result = new System.Text.StringBuilder();
-                result.AppendLine($"for current user - {this.UserName}\n");
+                result.AppendLine($"for current user - {_userName}\n");
                 result.AppendLine("Game's ID\t\tPoints\tStatus\tRating\t\tOpponent\tOpponent's points\tOpponent's Status");
-                foreach (var item in gameAccountStatus)
+                foreach (var item in _gameAccountStatus)
                 {
                     result.AppendLine($"{item.GameIdStr}\t\t{item.UserGameResultRating}\t{item.UserGameStatus}\t{item.MainUserRating}\t\t{item.OpponentUser.UserName}\t\t{item.OpponentGameResultRating}\t\t\t{item.OpponentGameStatus}");
                 }
@@ -53,20 +93,20 @@ namespace Lab1
 
         public virtual void WinGame(GameAccount opponent, BaseGame game)
         {
-            this.CurrentRating += game.GameRatingDefine();
-            PlayedGameResult(this, opponent, game, GameResultStatus.win, isVip, isCheater);
+            _currentRating += game.GameRating;
+            PlayedGameResult(this, opponent, game, GameResultStatus.win, _isVip, _isCheater);
         }
 
         public virtual void LooseGame(GameAccount opponent, BaseGame game)
         {
-            GetExeption(CheckRating(this, game.GameRating), this);
-            this.CurrentRating = (CheckRating(this, game.GameRatingDefine())) ? this.CurrentRating - game.GameRatingDefine() : 1;
-            PlayedGameResult(this, opponent, game, GameResultStatus.loose, isVip, isCheater);
+            GetExeption(CheckRating(game.GameRating));
+            _currentRating = (CheckRating(game.GameRating)) ? _currentRating - game.GameRating : 1;
+            PlayedGameResult(this, opponent, game, GameResultStatus.loose, _isVip, _isCheater);
         }
 
         public static void PlayedGameResult(GameAccount user, GameAccount opponent, BaseGame game, GameResultStatus grs, bool isVip, bool isCheater)
         {
-            user.gameAccountStatus.Add(new History(user, opponent, game, grs, isVip, isCheater));
+            user._gameAccountStatus.Add(new History(user, opponent, game, grs, isVip, isCheater));
         }
 
     }    
